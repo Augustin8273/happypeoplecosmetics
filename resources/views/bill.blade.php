6e2d5c08ee0cf12f5a075b62e577b5a94c1f9405
Augustin8273 <augubu9@gmail.com>
2023-11-18 20:55:55 +0200

    update

--- resources/views/bill.blade.php
+++ resources/views/bill.blade.php
@@ -34,6 +34,16 @@
        integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+       <style>
+        @media print {
+            #printButton,
+            .sidebar {
+                display: none;
+            }
+        }
+
+    </style>
+
 </head>
 
 <body>
