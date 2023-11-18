dad78cf8b698fc2e857e3b4c11aa980fc4a1da74
Augustin8273 <augubu9@gmail.com>
2023-11-18 18:44:08 +0200

    update

--- resources/views/sortis.blade.php
+++ resources/views/sortis.blade.php
@@ -282,7 +282,7 @@
                 <div class="col-lg-12">
                     <div class="row">
                         <div class="col-lg-2"></div>
-                        <div class="col-lg-6">
+                        <div class="col-lg-8">
                             <div class="row">
 
                                 <div class="card top-selling overflow-auto">
